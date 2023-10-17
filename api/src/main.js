95c0cc090c0d07ab3bb17c94e6cf9cf72027976c
Kareem Ayodeji <40839246+ayodeji-kareem@users.noreply.github.com>
2023-10-17 03:13:29 +0100

    installed serverless

---
+++ api/src/main.js
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import serverless from 'serverless-http';
+
+async function bootstrap () {
+  const app = await NestFactory.create (AppModule);
+  const globalPrefix = '.netlify/functions/main';
+  app.setGlobalPrefix (globalPrefix);
+  await app.init ();
+
+  const expressApp = app.getHttpAdapter ().getInstance ();
+  return serverless (expressApp)
+}
+
+let server;
+
+export const handler = async (event, context, callback) => {
+  server = server ?? (await bootstrap ());
+  return server (event, context, callback);
+};
